d5ed7124e648f4df643e58db61e43ccbf73edfbe
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2021-01-17 17:11:35 +0530

    add smooth scroll and grid

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ var zipCode = document.querySelector(".zipCode");
 var dateValue = document.querySelector(".date");
 const calculateButton = document.querySelector(".calculate");
 const results = document.querySelector(".results");
-const percentage = 0;
+var percentage = Math.floor(Math.random() * 99 + 1);
 
 function create() {
   var para = document.createElement("p");
   para.classList.add("para");
-  para.innerHTML = "2 Week prediction";
+  para.innerHTML = "2 Week Forecast";
   results.appendChild(para);
+  elem = document.createElement("hr");
+  elem.setAttribute("width", "80%");
+  results.appendChild(elem);
   dateValue = new Date(
     dateValue.value.substring(0, 4),
     dateValue.value.substring(5, 7) - 1,
@@ -28,9 +31,9 @@ function create() {
       newDiv.appendChild(newDay);
       var newLi = document.createElement("li");
       newLi.classList.add("percentage");
-      if (percentage < 20) {
+      if (percentage <= 30) {
         newLi.classList.add("green");
-      } else if (percentage < 75) {
+      } else if (percentage <= 60) {
         newLi.classList.add("yellow");
       } else {
         newLi.classList.add("red");
@@ -39,6 +42,7 @@ function create() {
       newDiv.appendChild(newLi);
       resultRow.appendChild(newDiv);
       dateValue.setDate(dateValue.getDate() + 1);
+      percentage = Math.floor(Math.random() * 100 + 1);
     }
     results.appendChild(resultRow);
   }
