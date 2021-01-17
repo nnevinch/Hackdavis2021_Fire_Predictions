43914360d232b39f0d3896a03c33dc728696b8ab
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2021-01-17 13:16:23 +0530

    add css for results

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ var zipCode = document.querySelector(".zipCode");
 var dateValue = document.querySelector(".date");
 const calculateButton = document.querySelector(".calculate");
 const results = document.querySelector(".results");
-const percentage = "0%";
+const percentage = 0;
 
 function create() {
-  console.log(dateValue.value);
   dateValue = new Date(
     dateValue.value.substring(0, 4),
     dateValue.value.substring(5, 7) - 1,
@@ -13,19 +12,31 @@ function create() {
   );
   console.log(dateValue);
   console.log(zipCode.value);
-  for (var i = 0; i < 7; i++) {
-    var newDiv = document.createElement("div");
-    newDiv.classList.add("day");
-    var newDay = document.createElement("li");
-    newDay.classList.add("dateInput");
-    newDay.innerHTML = dateValue.getMonth() + 1 + "/" + dateValue.getDate();
-    newDiv.appendChild(newDay);
-    var newLi = document.createElement("li");
-    newLi.classList.add("percentage");
-    newLi.innerHTML = percentage;
-    newDiv.appendChild(newLi);
-    results.appendChild(newDiv);
-    dateValue.setDate(dateValue.getDate() + 1);
+  for (var j = 0; j < 2; j++) {
+    for (var i = 0; i < 7; i++) {
+      var newDiv = document.createElement("div");
+      newDiv.classList.add("day");
+      var newDay = document.createElement("li");
+      newDay.classList.add("dateInput");
+      newDay.innerHTML = dateValue.getMonth() + 1 + "/" + dateValue.getDate();
+      newDiv.appendChild(newDay);
+      var newLi = document.createElement("li");
+      newLi.classList.add("percentage");
+      if (percentage < 20) {
+        newLi.classList.add("green");
+      } else if (percentage < 75) {
+        newLi.classList.add("yellow");
+      } else {
+        newLi.classList.add("red");
+      }
+      newLi.innerHTML = percentage + "%";
+      newDiv.appendChild(newLi);
+      results.appendChild(newDiv);
+      dateValue.setDate(dateValue.getDate() + 1);
+    }
+    var breakDiv = document.createElement("div");
+    breakDiv.classList.add("breakDiv");
+    results.appendChild(breakDiv);
   }
   return;
 }
